25a614c019ccf736c1d5383124dc544c14789e6d
Shadow's Peak <99706791+Shadows-Peak@users.noreply.github.com>
2024-04-24 09:26:06 -0700

    Create TestboltSpell.java

---
+++ src/main/java/io/redspace/ironsspellbooks/spells/fire/TestboltSpell.java
@@ -0,0 +1,82 @@
+package io.redspace.ironsspellbooks.spells.fire;
+
+import io.redspace.ironsspellbooks.IronsSpellbooks;
+import io.redspace.ironsspellbooks.api.config.DefaultConfig;
+import io.redspace.ironsspellbooks.api.magic.MagicData;
+import io.redspace.ironsspellbooks.api.registry.SchoolRegistry;
+import io.redspace.ironsspellbooks.api.spells.*;
+import io.redspace.ironsspellbooks.api.util.Utils;
+import io.redspace.ironsspellbooks.damage.ISpellDamageSource;
+import io.redspace.ironsspellbooks.entity.spells.testbolt.TestboltProjectile;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+import java.util.Optional;
+
+@AutoSpellConfig
+public class TestboltSpell extends AbstractSpell {
+    private final ResourceLocation spellId = new ResourceLocation(IronsSpellbooks.MODID, "testbolt");
+
+    @Override
+    public List<MutableComponent> getUniqueInfo(int spellLevel, LivingEntity caster) {
+        return List.of(Component.translatable("ui.irons_spellbooks.damage", Utils.stringTruncation(getDamage(spellLevel, caster), 2)));
+    }
+
+    private final DefaultConfig defaultConfig = new DefaultConfig()
+            .setMinRarity(SpellRarity.COMMON)
+            .setSchoolResource(SchoolRegistry.FIRE_RESOURCE)
+            .setMaxLevel(10)
+            .setCooldownSeconds(1)
+            .build();
+
+    public TestboltSpell() {
+        this.manaCostPerLevel = 2;
+        this.baseSpellPower = 12;
+        this.spellPowerPerLevel = 1;
+        this.castTime = 0;
+        this.baseManaCost = 10;
+    }
+
+    @Override
+    public CastType getCastType() {
+        return CastType.INSTANT;
+    }
+
+    @Override
+    public DefaultConfig getDefaultConfig() {
+        return defaultConfig;
+    }
+
+    @Override
+    public ResourceLocation getSpellResource() {
+        return spellId;
+    }
+
+    @Override
+    public void onCast(Level world, int spellLevel, LivingEntity entity, CastSource castSource, MagicData playerMagicData) {
+        TestboltProjectile testbolt = new TestboltProjectile(world, entity);
+        testbolt.setPos(entity.position().add(0, entity.getEyeHeight() - testbolt.getBoundingBox().getYsize() * .5f, 0));
+        testbolt.shoot(entity.getLookAngle());
+        testbolt.setDamage(getDamage(spellLevel, entity));
+        world.addFreshEntity(testbolt);
+        super.onCast(world, spellLevel, entity, castSource, playerMagicData);
+    }
+
+    @Override
+    public DamageSource getDamageSource(@Nullable Entity projectile, Entity attacker) {
+        return ((ISpellDamageSource) super.getDamageSource(projectile, attacker)).setFireTime(3).get();
+    }
+
+    private float getDamage(int spellLevel, LivingEntity entity) {
+        return getSpellPower(spellLevel, entity) * .5f;
+    }
+
+}
